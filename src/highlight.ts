0156775108de5746c8d81f30f2c94a9c9757440b
asceznyk <asceznyk@gmail.com>
2023-08-10 16:33:35 +0530

    retabbed and added spaces

--- src/highlight.ts
+++ src/highlight.ts
@@ -2,84 +2,82 @@ import { parser } from '@lezer/python';
 import { TreeCursor } from '@lezer/common';
 
 export function highlightSyntax (
-	program:string, 
-	renderDiv:HTMLElement, 
-	lineHeight:number
+  program:string, 
+  renderDiv:HTMLElement, 
+  lineHeight:number
 ) {
 
-	function isAlphaNumeric(str:string): boolean {
-		let ascii, i, len;
-		for (i = 0, len = str.length; i < len; i++) {
-			ascii = str.charCodeAt(i);
-			if (
-				!(ascii > 47 && ascii < 58) && // numeric (0-9)
-				!(ascii > 64 && ascii < 91) && // upper alpha (A-Z)
-				!(ascii > 96 && ascii < 123) // lower alpha (a-z)
-			) { 
-				return false;
-			}
-		}
-		return true;
-	};
+  function isAlphaNumeric(str:string): boolean {
+    let ascii, i, len;
+    for (i = 0, len = str.length; i < len; i++) {
+      ascii = str.charCodeAt(i);
+      if (
+        !(ascii > 47 && ascii < 58) && // numeric (0-9)
+        !(ascii > 64 && ascii < 91) && // upper alpha (A-Z)
+        !(ascii > 96 && ascii < 123) // lower alpha (a-z)
+      ) return false;
+    }
+    return true;
+  };
 
-	function createLine(): HTMLElement {
-		let lDiv = document.createElement("div");
-		lDiv.setAttribute('style', `position:relative; height:${lineHeight}px;`); 
-		renderDiv.appendChild(lDiv);
-		return lDiv;
-	}
+  function createLine(): HTMLElement {
+    let lDiv = document.createElement("div");
+    lDiv.setAttribute('style', `position:relative; height:${lineHeight}px;`); 
+    renderDiv.appendChild(lDiv);
+    return lDiv;
+  }
 
-	function fillWhiteSpace(from:number, to:number) {
-		for(let i = from; i < to; i++) { 
-			if(program[i] == '\n') lineDiv = createLine();
-			else if (program[i] == ' ')  lineDiv.innerHTML += '&nbsp';
-		}
-	}
+  function fillWhiteSpace(from:number, to:number) {
+    for(let i = from; i < to; i++) { 
+      if(program[i] == '\n') lineDiv = createLine();
+      else if (program[i] == ' ')  lineDiv.innerHTML += '&nbsp';
+    }
+  }
 
-	function getKWType(node:any, clsname:string, value:string): string {
-		cls = clsname.toLowerCase();
+  function getKWType(node:any, clsname:string, value:string): string {
+    cls = clsname.toLowerCase();
 
-		if(cls == value) cls = isAlphaNumeric(value) ?  "keyword" : "seperator";
-		else if(cls.endsWith("op")) cls = "operator";
-		else if( 
-			node._parent != null &&
-			node._parent.type.clsname == 'FunctionDefinition' && 
-			clsname == 'VariableName'
-		) cls = "def";
+    if(cls == value) cls = isAlphaNumeric(value) ?  "keyword" : "seperator";
+    else if(cls.endsWith("op")) cls = "operator";
+    else if( 
+      node._parent != null &&
+      node._parent.type.clsname == 'FunctionDefinition' && 
+      clsname == 'VariableName'
+    ) cls = "def";
 
-		return cls;
-	}
+    return cls;
+  }
 
-	function fillNodeColor() { 
-		let [from, to] = [treeCursor.from, treeCursor.to];
-		let kw = getKWType(treeCursor.node, treeCursor.name, program.slice(from, to)) ;
-		let kwSpan = document.createElement("span");
+  function fillNodeColor() { 
+    let [from, to] = [treeCursor.from, treeCursor.to];
+    let kw = getKWType(treeCursor.node, treeCursor.name, program.slice(from, to)) ;
+    let kwSpan = document.createElement("span");
 
-		kwSpan.classList.add(`py-${kw}`);
-		for(let i = from; i < to; i++) {
-			if(program[i] == '\n') {
-				lineDiv.appendChild(kwSpan);
-				kwSpan = document.createElement("span");
-				kwSpan.classList.add(`py-${kw}`);
-				lineDiv = createLine();
-				continue;
-			} 
-			kwSpan.innerHTML += program[i];
-		}
-		lineDiv.appendChild(kwSpan); 
-	}
+    kwSpan.classList.add(`py-${kw}`);
+    for(let i = from; i < to; i++) {
+      if(program[i] == '\n') {
+        lineDiv.appendChild(kwSpan);
+        kwSpan = document.createElement("span");
+        kwSpan.classList.add(`py-${kw}`);
+        lineDiv = createLine();
+        continue;
+      } 
+      kwSpan.innerHTML += program[i];
+    }
+    lineDiv.appendChild(kwSpan); 
+  }
 
-	renderDiv.innerHTML = ''; 
-	let treeCursor:TreeCursor = parser.parse(program).cursor();
-	let lineDiv:HTMLElement;
-	let prevPoint:number = 0;
-	let cls:string;
+  renderDiv.innerHTML = ''; 
+  let treeCursor:TreeCursor = parser.parse(program).cursor();
+  let lineDiv:HTMLElement;
+  let prevPoint:number = 0;
+  let cls:string;
 
-	lineDiv = createLine();
-	while(treeCursor.next()) {
-		if(treeCursor.node.firstChild != null) continue;
-		fillWhiteSpace(prevPoint, treeCursor.from);
-		fillNodeColor();
-		prevPoint = treeCursor.to;
-	}
+  lineDiv = createLine();
+  while(treeCursor.next()) {
+    if(treeCursor.node.firstChild != null) continue;
+    fillWhiteSpace(prevPoint, treeCursor.from);
+    fillNodeColor();
+    prevPoint = treeCursor.to;
+  }
 }
--- src/render.ts
+++ src/render.ts
@@ -11,89 +11,89 @@ const capture = document.querySelector<HTMLInputElement>("#capture")!;
 const cursor = document.querySelector<HTMLElement>("#cursor")!;
 
 (function () {
-	function followCursor() {
-		let value = capture.value.slice(0, capture.selectionStart as number); 
-		let matches = [...value.matchAll(/\n/g)];
-		let cy = matches.length;
+  function followCursor() {
+    let value = capture.value.slice(0, capture.selectionStart as number); 
+    let matches = [...value.matchAll(/\n/g)];
+    let cy = matches.length;
 
-		let cx = value.length;
-		if(cy > 0) { 
-			cx = value.slice(matches[cy-1].index, value.length).replace('\n', '').length; 
-		}
+    let cx = value.length;
+    if(cy > 0) { 
+      cx = value.slice(matches[cy-1].index, value.length).replace('\n', '').length; 
+    }
 
-		cursor.setAttribute('style', `top:${cy*vscl}px; left:${(cx+lineOffset+1)*hscl}px`);
-	}
+    cursor.setAttribute('style', `top:${cy*vscl}px; left:${(cx+lineOffset+1)*hscl}px`);
+  }
 
-	function isArrow(keycode:number): boolean {
-		return (keycode == 37 || keycode == 38 || keycode == 39 || keycode == 40) 
-	}
+  function isArrow(keycode:number): boolean {
+    return (keycode == 37 || keycode == 38 || keycode == 39 || keycode == 40) 
+  }
 
-	function validateKey(keycode:number): boolean {
-		let valid = 
-			(keycode > 47 && keycode < 58) || // number keys
-			(keycode == 32 || keycode == 13 || keycode == 8 || keycode == 9) ||
-			isArrow(keycode) ||
-			(keycode > 64 && keycode < 91) || // letter keys
-			(keycode > 95 && keycode < 112) || // numpad keys
-			(keycode > 185 && keycode < 193) || // ;=,-./` (in order)
-			(keycode > 218 && keycode < 223);   // [\]' (in order)
-		return valid;
-	}
+  function validateKey(keycode:number): boolean {
+    let valid = 
+      (keycode > 47 && keycode < 58) || // number keys
+      (keycode == 32 || keycode == 13 || keycode == 8 || keycode == 9) ||
+      isArrow(keycode) ||
+      (keycode > 64 && keycode < 91) || // letter keys
+      (keycode > 95 && keycode < 112) || // numpad keys
+      (keycode > 185 && keycode < 193) || // ;=,-./` (in order)
+      (keycode > 218 && keycode < 223);   // [\]' (in order)
+    return valid;
+  }
 
-	function setLineNums(program:string) {
-		let numLines = program.split('\n').length;
-		lineOffset = numLines.toString().length + 1;
+  function setLineNums(program:string) {
+    let numLines = program.split('\n').length;
+    lineOffset = numLines.toString().length + 1;
 
-		lines.innerHTML = '';
-		for(let i = 0; i < numLines; i++) {
-			let offLine = '';
-			for(let k = 0; k < lineOffset - (i+1).toString().length; k++) offLine += '&nbsp;';
-			lines.innerHTML += `<div class='line-num' style='height:${vscl}px;'>${offLine}${i+1}&nbsp;</div>`;
-		}
+    lines.innerHTML = '';
+    for(let i = 0; i < numLines; i++) {
+      let offLine = '';
+      for(let k = 0; k < lineOffset - (i+1).toString().length; k++) offLine += '&nbsp;';
+      lines.innerHTML += `<div class='line-num' style='height:${vscl}px;'>${offLine}${i+1}&nbsp;</div>`;
+    }
 
-		let wOff:number = (lineOffset+1)*hscl;
-		lines.setAttribute('style', `width:${wOff}px`);
-		code.setAttribute('style', `width:calc(100% - ${wOff}px); height:${lines.offsetHeight}px;`);
-	}
+    let wOff:number = (lineOffset+1)*hscl;
+    lines.setAttribute('style', `width:${wOff}px`);
+    code.setAttribute('style', `width:calc(100% - ${wOff}px); height:${lines.offsetHeight}px;`);
+  }
 
-	let lineOffset:number;
-	let initOff:number = 3*hscl;
+  let lineOffset:number;
+  let initOff:number = 3*hscl;
 
-	lines.innerHTML = `<div class='line-num' style='height:${vscl}px'>&nbsp;1&nbsp;</div>`;
-	lines.setAttribute('style', `width:${initOff}px`);
-	code.setAttribute('style', `width:calc(100% - ${initOff}px)`);
+  lines.innerHTML = `<div class='line-num' style='height:${vscl}px'>&nbsp;1&nbsp;</div>`;
+  lines.setAttribute('style', `width:${initOff}px`);
+  code.setAttribute('style', `width:calc(100% - ${initOff}px)`);
 
-	cursor.setAttribute('style',`left:${initOff}px`);
-	code.addEventListener("click", () => {
-		capture.focus();
-	})
+  cursor.setAttribute('style',`left:${initOff}px`);
+  code.addEventListener("click", () => {
+    capture.focus();
+  })
 
-	capture.addEventListener("keyup", (e:KeyboardEvent) => {
-		let keycode:number = e.keyCode;
-		if(validateKey(keycode)) {
-			if(!isArrow(keycode)) {
-				let val = capture.value;
-				setLineNums(val);
-				highlightSyntax(val, code, vscl);
-			} 
-		}
-		followCursor();
-	})
+  capture.addEventListener("keyup", (e:KeyboardEvent) => {
+    let keycode:number = e.keyCode;
+    if(validateKey(keycode)) {
+      if(!isArrow(keycode)) {
+        let val = capture.value;
+        setLineNums(val);
+        highlightSyntax(val, code, vscl);
+      } 
+    }
+    followCursor();
+  })
 
-	capture.addEventListener("keydown", (e:KeyboardEvent) => {
-		let keycode:number = e.keyCode;
-		if(validateKey(keycode)) {
-			if(keycode == 9) {
-				e.preventDefault();
-				let tgt = e.target as HTMLInputElement;
-				let start:number = tgt.selectionStart!;
-				let end:number = tgt.selectionEnd!;
-				tgt.value = tgt.value.substring(0, start) + tabStr + tgt.value.substring(end);
-				tgt.selectionStart = tgt.selectionEnd = start + tabStr.length;
-			} 
-			followCursor();
-		}
-	})
+  capture.addEventListener("keydown", (e:KeyboardEvent) => {
+    let keycode:number = e.keyCode;
+    if(validateKey(keycode)) {
+      if(keycode == 9) {
+        e.preventDefault();
+        let tgt = e.target as HTMLInputElement;
+        let start:number = tgt.selectionStart!;
+        let end:number = tgt.selectionEnd!;
+        tgt.value = tgt.value.substring(0, start) + tabStr + tgt.value.substring(end);
+        tgt.selectionStart = tgt.selectionEnd = start + tabStr.length;
+      } 
+      followCursor();
+    }
+  })
 })();
 
 
